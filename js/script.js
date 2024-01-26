c87f9d2d0ac7d5236b0e9719c759feab7de8f4f8
mattia_morriale <mattia.morriale1@gmail.com>
2024-01-26 14:54:35 +0100

    fine esercizio con bonus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-const km = prompt("quanti chilometri vuoi percorrere?");
+const km = Number(prompt("Quanti chilometri vuoi percorrere?"));
+
+document.getElementById("km").innerHTML = km;
+
+const age = Number(prompt("Quanti anni hai?"));
+
+document.getElementById("age").innerHTML = age;
 
 const prezzo = 0.21;
 
+document.getElementById("prezzo").innerHTML = prezzo;
+
 const pfinale = prezzo * km;
 
+document.getElementById("pfinale").innerHTML = (pfinale).toFixed(2);
 
-console.log
+if ( age < 18 ) {
+
+    document.getElementById("pfinale").innerHTML = (pfinale - ( pfinale * 20 / 100 )).toFixed(2);
+
+} else if ( age > 64 ) {
+
+    document.getElementById("pfinale").innerHTML = (pfinale - ( pfinale * 40 / 100 )).toFixed(2);
+
+}
